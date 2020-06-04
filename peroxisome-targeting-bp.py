cbbd1b24200b5db3459165231610f89b932ecfaa
Josh Dyer <jd583@exeter.ac.uk>
2020-06-04 17:18:40 +0100

    In process of adding batch functionality - struggling to get mask to append to final df

--- peroxisome-targeting-bp.py
+++ peroxisome-targeting-bp.py
@@ -1,23 +1,34 @@
 import gzip
 from Bio import SeqIO
 import pandas as pd
+import glob
 
-# open a list for the identifyer and for the sequence
-identifyer = []
-sequence = []
-# open faa.gz file
-with gzip.open(r'C:/Users/jdeed/OneDrive - University of Exeter/Peroxisome-Targeted BPs/Bacterial sequence files/mycobacteria protein fasta files/ncbi-genomes-2020-06-03/GCF_010731775.1_ASM1073177v1_protein.faa.gz',
-           "rt") as handle:
-    # now append the sequence and identifyer to the open lists
-    for record in SeqIO.parse(handle, "fasta"):
-        identifyer.append(record.id)
-        # note that I have converted the fasta to a string here (else it
-        # adds a comma between each amino acid)
-        sequence.append(str(record.seq))
-#append to a df then change column names and set the index to the identifyer
-df = pd.DataFrame(data = [identifyer,sequence]).T
-df.columns = ['Identifyer','Sequence']
-df.set_index("Identifyer", inplace = True)
-df.head(5)
-SKLmask = df["Sequence"].str.upper().str.endswith("SKL")
-print(df[SKLmask])
+filelist = glob.glob(r'C:\Users\jdeed\OneDrive - University of Exeter\Peroxisome-Targeted BPs\Bacterial sequence files\mycobacteria protein fasta files\test' 
+          + "/*protein.faa.gz")
+
+collateddfcolumns = ["Identifyer","Sequence"]
+collated_df = pd.DataFrame(columns = collateddfcolumns)
+collated_df.set_index("Identifyer", inplace = True)
+print(collated_df)
+
+for file in filelist:     
+    # open a list for the identifyer and for the sequence
+    identifyer = []
+    sequence = []
+    # open faa.gz file
+    with gzip.open(file, "rt") as handle:
+        # now append the sequence and identifyer to the open lists
+        for record in SeqIO.parse(handle, "fasta"):
+            identifyer.append(record.id)
+            # note that I have converted the fasta to a string here (else it
+            # adds a comma between each amino acid)
+            sequence.append(str(record.seq))
+    #append to a df then change column names and set the index to the identifyer
+    df = pd.DataFrame(data = [identifyer,sequence]).T
+    df.columns = ['Identifyer','Sequence']
+    df.set_index("Identifyer", inplace = True)
+    SKLmask = df["Sequence"].str.upper().str.endswith("SKL")
+    skldf = df[SKLmask]
+    print (skldf)
+    collated_df.append(skldf)           
+print(collated_df)
