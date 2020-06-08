00f23c965b876230810aae944e7cb4970eccd343
Josh Dyer <jd583@exeter.ac.uk>
2020-06-08 11:18:23 +0100

    removed gene_annotation_retrieval.py

--- gene_annotation_retrieval.py
+++
@@ -1,9 +0,0 @@
-from Bio import SeqIO
-import pandas as pd
-
-inputfile = r'C:/Users/jdeed/OneDrive - University of Exeter/Peroxisome-Targeted BPs/Bacterial sequence files/test/test_positive_SKL.csv'
-
-df = pd.read_csv(inputfile)
-print(df)
-
-print(df.dtypes)
