c79daef23d849b45d2ef26d34ba408be3c020282
Josh Dyer <jd583@exeter.ac.uk>
2020-06-17 12:32:26 +0100

    counted number of genes using countnumber of genes.py, updated readme.md

--- count number of genes.py
+++ count number of genes.py
@@ -8,37 +8,28 @@ inputfolder = r'D:\Bacterial Protein Sequences\ncbi-genomes-2020-06-05'
 #output folder
 outputpath = r'D:\Bacterial Protein Sequences\ncbi-genomes-2020-06-05'
 # output filename
-totaldfoutputname = '2020-06-05 Total bacterial genes'
-nuniqueoutputname = '2020-06-05 Number of unique genes'
+outputname = '2020-06-05 Total number of genes'
+
 # make empty df to append results to
 collateddfcolumns = ["Identifyer","Description","Sequence"]
 collated_df = pd.DataFrame(columns = collateddfcolumns)
-
 filelist = glob.glob(inputfolder + '/*protein.faa.gz')
+# set gene counter to 0
+genecounter = 0
+#open list 
 for i, file in enumerate(filelist):
-    identifyer = []
-    sequence = []
-    description = []
+    genelist = []
     print('protcessing file {}, this is file {} of {}'.format(file, i +1, len(filelist)))
     with gzip.open(file, "rt") as handle:
-        # now append the sequence and identifyer to the open lists
+        # now append the identifyer to the open lists
         for record in SeqIO.parse(handle, "fasta"):
-            identifyer.append(record.id)
-            # note that I have converted the fasta to a string here (else it
-            # adds a comma between each amino acid)
-            sequence.append(str(record.seq))
-            description.append(record.description)
-       #append to a df then change column names and set the index to the identifyer
-# note that .T transposes the data 
-        df = pd.DataFrame(data = [identifyer,description,sequence]).T
-        df.columns = ['Identifyer','Description','Sequence']
-        # append to collated df
-        collated_df = collated_df.append(df)   
-# save this df containing every bacterial gene
-collated_df.to_csv("{}/{}.csv".format(outputpath,totaldfoutputname), header = True, index = None)
-# Determine the number of unique values and save that to a series and export as
-# .csv
-unique = collated_df.nunique()
-uniqueseries = pd.Series(unique)
-uniqueseries.to_csv("{}/numberuniquevalues.csv".format(outputpath))
+            genelist.append(record.id)
+    # count number of genes per file
+    genesperfile = len(genelist)
+    # add this number to counter
+    genecounter = genecounter + genesperfile
 
+# convert number of genes to a series for easy export
+genenumberseries = pd.Series(genecounter)
+# save this series containing every bacterial gene
+genenumberseries.to_csv("{}/{}.csv".format(outputpath,outputname), header = True, index = None)
--- readme.md
+++ readme.md
@@ -1,2 +1,11 @@
 __Identification of bacterial proteins containing the peroxisome
 targeting motif__
+
+_order of analysis_
+
+- First performed a search for sequences with "SKL" at the N-terminus using the "peroxisome-targeting-bp.py"
+
+- Then cleaned the data using the "peroxisome-targeting-bp-dataset-manip.py" code
+  - UPDATE 17062020 - I need to redo this to make sure that only unique seuqneces are included in the final dataset. - I will screen for this on the basis of gene_ID first - the problem with screening on the basis of sequence for unique sequences is the (low) possibility of missing out a gene present in two seperate bacteria that include the exact same
+
+```
